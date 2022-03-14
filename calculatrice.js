c57c5405e893f85cfe7d00af0f0b115eda21e7a4
zinebaboumejd <zinebaboumejd@gmail.com>
2022-03-14 16:27:17 +0100

    calculatrice

---
+++ calculatrice.js
@@ -0,0 +1,8 @@
+new Vue ({
+el:"#app",
+date:{
+    nombre1 :0,
+    nombre2 :0,
+    result :0
+}
+});
--- main.js
+++ main.js
@@ -9,14 +9,44 @@ new Vue({
         //    'padding':'10px',
         // },
         // bg: true
+// **********P2**********************
+        // name:'',
+        // names:['amal' ,'sara ','salma','soukaina']
+        //  },
+        //  methods:
+        //  {
+        //      addName(){
+        //          this.names.push('Houda');
+        //      }
+// ***************P3:calculatrice*******************
+       nombre1 :0,
+       nombre2 :0,
+       result :0
+          },
+          methods:{
+                  addition(){
+                          return this.result = parseInt(this.nombre1)+parseInt(this.nombre2);
+                  },
+                  sustraction(){
+                        return this.result = parseInt(this.nombre1)-parseInt(this.nombre2);
+                  },
+                  multiplication(){
+                        return this.result = parseInt(this.nombre1)*parseInt(this.nombre2);
+                  },
+                  division(){
+                        if(this.nombre1==0 || this.nombre2==0){
+                                return this.result=0;
+                        }else{
+                                return this.result =Math.round(parseInt(this.nombre1)/parseInt(this.nombre2));
+                              //  return this.result =Math.floor(parseInt(this.nombre1)/parseInt(this.nombre2));
+                    }
+                  },
+                  CE(){
+                          this.nombre1=0;
+                          this.nombre2=0;
+                          this.result=0;
+                  }
 
-        name:'',
-        names:['amal' ,'sara ','salma','soukaina']
-         },
-         methods:
-         {
-             addName(){
-                 this.names.push('Houda');
-             }
-         }
+
+          }
 });
