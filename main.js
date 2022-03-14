b31476a6ab6847341e20c73b0e35545955c9373f
zinebaboumejd <zinebaboumejd@gmail.com>
2022-03-14 11:24:47 +0100

    p1

---
+++ main.js
@@ -0,0 +1,13 @@
+new Vue({
+    el:"#app",
+    data:{
+        message: 'Bonjour,1er test vue js',
+        notshow:'au revoir',
+        show:false,
+        divStyle:{
+           'background-color':'#A7E0E0',
+           'padding':'10px',
+        },
+        bg: true
+         },
+});
